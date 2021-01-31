edaf00383c1d49efcd2c5553e1e137cacf7f2fc5
Rhys <rhysbiggin@gmail.com>
2021-01-31 10:40:32 +0000

    Basic implementation of model mapper
    - contains duplication that could be removed

--- app/src/main/java/com/briggin/footballfinder/presentation/ModelMapper.kt
+++ app/src/main/java/com/briggin/footballfinder/presentation/ModelMapper.kt
@@ -1,9 +1,50 @@
 package com.briggin.footballfinder.presentation
 
-import com.briggin.footballfinder.domain.Result
+import com.briggin.footballfinder.R
+import com.briggin.footballfinder.domain.*
+import java.lang.StringBuilder
 
 class ModelMapper {
 
-    fun mapToModels(domain: Result): List<FootballModel> = emptyList()
+    fun mapToModels(domain: Result): List<FootballModel> =
+        domain.playerItems() + domain.teamItems()
 
+    private fun Result.playerItems(): List<FootballModel> = when (players) {
+        is Complete -> mutableListOf<FootballModel>().apply {
+            add(Header(R.string.header_players))
+            addAll(players.domains.map { it.toModel() })
+        }
+        is Partial -> mutableListOf<FootballModel>().apply {
+            add(Header(R.string.header_players))
+            addAll(players.domains.map { it.toModel() })
+            add(LoadMore(ModelType.LoadMorePlayers, R.string.load_more_players))
+        }
+        is NoDomainsFound -> emptyList()
+    }
+
+    private fun PlayerDomain.toModel(): Player = Player(
+        id,
+        StringBuilder().apply {
+            if (firstName.isNotBlank()) append("$firstName ")
+            append(secondName)
+        }.toString(),
+        age.toString(),
+        club,
+        isFavourite
+    )
+
+    private fun Result.teamItems(): List<FootballModel> = when (teams) {
+        is Complete -> mutableListOf<FootballModel>().apply {
+            add(Header(R.string.header_teams))
+            addAll(teams.domains.map { it.toModel() })
+        }
+        is Partial -> mutableListOf<FootballModel>().apply {
+            add(Header(R.string.header_teams))
+            addAll(teams.domains.map { it.toModel() })
+            add(LoadMore(ModelType.LoadMoreTeams, R.string.load_more_teams))
+        }
+        is NoDomainsFound -> emptyList()
+    }
+
+    private fun TeamDomain.toModel(): Team = Team(name, city, stadium)
 }
